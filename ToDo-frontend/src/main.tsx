2455b3fbf67e4f9012147643fac2579282d76cfe
Pavan Kumar <105868944+pavan-kumar2@users.noreply.github.com>
2026-08-25 22:38:19 +0530

    refactor: restructure main component to include routing and navigation

--- ToDo-frontend/src/main.tsx
+++ ToDo-frontend/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 import CreateTodo from "./components/CreateTodo";
+import { BrowserRouter, Route, Routes } from "react-router";
+import TodoList from "./components/TodoList";
+import Navigation from "./components/Navigation";
+
+
 
 createRoot(document.getElementById("app")!).render(
     <StrictMode>
-        <CreateTodo />
+        <BrowserRouter>
+            <Navigation />
+            <Routes>
+                <Route path="/" element={<TodoList />} />
+                <Route path="/create" element={<CreateTodo />} />
+            </Routes>
+        </BrowserRouter>
     </StrictMode>
 );
